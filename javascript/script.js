b7ca5e60119af73e0a9a18a9a1528512f08b8ed7
Marco Rampin <rampin.marco.17@gmail.com>
2022-12-09 12:05:21 +0100

    Testing with intro images from gitHub url

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
-  const leftPic = document.querySelector('#intro-left');
-  const rightPic = document.querySelector('#intro-right');
+  	const leftPic = document.querySelector('#intro-left');
+  	const rightPic = document.querySelector('#intro-right');
 
-  //creating an array with two random values (between 0 and 6)
-  const randomPic = () => {
-      const array = [];
-      for (let i=0; i<2; i++){
-        array.push(parseInt(Math.random()*6));
-    }
-    return array;
-  }
+  	//creating an array with two random values (between 0 and 6)
+  	const randomPic = () => {
+      		const array = [];
+      		for (let i=0; i<2; i++){
+        		array.push(parseInt(Math.random()*7));
+    		}
+    		return array;
+  	}
 
-  //setting the intro left and right random pics
-  const setPics = () => {
-      const pics = randomPic();
-      const left = "https://chiswick.blob.core.windows.net/blogs/Chiswick-Auctions-London-Kawase-Hasui-woodblock-print.jpg";
-	const right = "https://raw.githubusercontent.com/marcorampin/haiku/main/javascript/sky.jpg";
-      $(leftPic).attr("src", left);
-      $(rightPic).attr("src", right); 
-}
+	//setting the intro left and right random pics
+	const setPics = () => {
+		const pics = randomPic();
+      		const left = pics[0];
+		const right = pics[1];
+		const leftUrl = "https://raw.githubusercontent.com/marcorampin/haiku/main/images/intro/"+str(left)+".jpg";
+		const rightUrl = "https://raw.githubusercontent.com/marcorampin/haiku/main/images/intro/"+str(right)+".jpg";
+      		$(leftPic).attr("src", leftUrl);
+      		$(rightPic).attr("src", rightUrl); 
+	}
 
 
-  window.addEventListener('load', setPics);
+  	window.addEventListener('load', setPics);
 });
